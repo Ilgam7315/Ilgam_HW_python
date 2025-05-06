f758f86d847e5c8480c6d02bf3cc641cedeae7bb
Ilgam7315 <ilgam7315@gmail.com>
2025-05-06 21:03:24 +0500

    lesson_02_task_05

---
+++ lesson_02/lesson_02_task_05.py
@@ -0,0 +1,16 @@
+def month_to_season(month):
+    if  1 <= month <= 2 or 12 == month:
+        return "зима"
+    elif 6 <= month <= 8:
+        return "лето"
+    elif 9 <= month <= 11:
+        return "осень"
+    elif 3 <= month <= 5:
+        return "весна"
+    else:
+        return "Неверный номер месяца"
+try:
+    month = int(input("Введите номер месяца (от 1 до 12): "))
+    print(month_to_season(month))
+except ValueError:
+    print("Введите целое число от 1 до 12!")
