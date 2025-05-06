39fa715a9383865db3af0ee2938a306ef94343bf
Ilgam7315 <ilgam7315@gmail.com>
2025-05-06 22:54:31 +0500

    lesson_02_task_04

--- lesson_02/lesson_02_task_04.py
+++ lesson_02/lesson_02_task_04.py
@@ -1,12 +1,12 @@
 n = int(input("Введите число:"))
 def fizz_buzz(n):
     for num in range(1, n + 1):
-        if num % 3 == 0:
-            print(f"Fizz")
+        if num % 15 == 0:
+            print(f"FizzBuzz")
         elif num % 5 == 0:
             print(f"Buzz")
-        elif num % 15 == 0:
-            print(f"FizzBuzz")
+        elif num % 3 == 0:
+            print(f"Fizz")
         else:
             print(num)
 fizz_buzz(n)
