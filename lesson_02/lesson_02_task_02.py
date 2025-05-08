b0aed936ce566ac3730bde8f4a6c73c7d7cd1eb8
Ilgam7315 <ilgam7315@gmail.com>
2025-05-08 08:25:32 +0500

    После проверки НАСТАВНИКА

--- lesson_02/lesson_02_task_02.py
+++ lesson_02/lesson_02_task_02.py
@@ -1,7 +1,10 @@
 def is_year_leap(number):
-    return True if number % 4 == 0 else False
+    return number % 4 == 0
 
 
 num = int(input("Введите год: "))
 result = is_year_leap(num)
 print(f"год {num}: - {result}")
+
+# вместо return True if number % 4 == 0 else False можно
+# сократить и написать: return number % 4 == 0
--- lesson_02/lesson_02_task_05.py
+++ lesson_02/lesson_02_task_05.py
@@ -1,12 +1,12 @@
 def month_to_season(month):
-    if 1 <= month <= 2 or 12 == month:
+    if 1 <= month <= 2 or month == 12:
         return "зима"
+    elif 3 <= month <= 5:
+        return "весна"
     elif 6 <= month <= 8:
         return "лето"
     elif 9 <= month <= 11:
         return "осень"
-    elif 3 <= month <= 5:
-        return "весна"
     else:
         return "Неверный номер месяца"
 
